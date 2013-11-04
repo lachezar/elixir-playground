e98aecd34298d2cd4c0410fdc8a971753220a087
lucho870601 <lucho870601@gmail.com>
2013-11-05 00:23:43 +0100

    Rewrote the calcaas to run on separated nodes.

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Calculator as a Service. A tiny project to learn and play with Elixir.
 
 Right now it does not do much - just one process that calculates Fibonacci sequence.
+
+To start the program you need to open two terminals in the root folder and execute:
+
+```
+# Terminal 1
+iex --sname "fib" --cookie "secret" -S mix
+Calcaas.start_fib
+```
+
+```
+# Terminal 2
+iex --sname "ask" --cookie "secret" -S mix
+Node.connect :"fib@<your computer hostname>"
+Calcaas.start_curiosity
+```
--- lib/calcaas.ex
+++ lib/calcaas.ex
@@ -1,9 +1,31 @@
 defmodule Calcaas do
 
-  @behavior :application
-  def start do
-    {:ok, pid} = :gen_server.start_link(Fib, 0, [])
-    AskNext.start(pid)
+  def fib(m) do
+    receive do
+      {:next, {pid, n}} ->
+        IO.puts "... #{n + m}"
+        pid <- {:answer, n + m}
+    end
+    fib(n)
+  end
+
+  def ask(n, fib_pid) do
+    IO.puts "What goes after #{n}"
+    fib_pid <- {:next, {self(), n}}
+    receive do
+      {:answer, m} -> n = m       
+    end
+    ask(n, fib_pid)
+  end
+
+  def start_fib do
+    pid = spawn(__MODULE__, :fib, [0])
+    :global.register_name(:fib, pid)
+  end
+
+  def start_curiosity do
+    fib_pid = :global.whereis_name(:fib)
+    spawn(__MODULE__, :ask, [1, fib_pid]) 
   end
 end
 
